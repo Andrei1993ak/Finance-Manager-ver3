928e6b56d2d71c75d5825734f5745846a24c41bc
Andrei Kozlowsky from home <andri19.93ak@gmail.com>
2016-11-11 17:14:17 +0300

    Doing demo date chart

--- Finance3.0/app/src/main/java/com/gmail/a93ak/andrei19/finance30/view/reports/BalanceChartActivity.java
+++ Finance3.0/app/src/main/java/com/gmail/a93ak/andrei19/finance30/view/reports/BalanceChartActivity.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatSpinner;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 
 import com.gmail.a93ak.andrei19.finance30.R;
 import com.gmail.a93ak.andrei19.finance30.control.base.OnTaskCompleted;
+import com.gmail.a93ak.andrei19.finance30.control.base.RequestHolder;
 import com.gmail.a93ak.andrei19.finance30.control.base.Result;
 import com.gmail.a93ak.andrei19.finance30.control.executors.PurseExecutor;
 import com.gmail.a93ak.andrei19.finance30.model.models.Purse;
@@ -17,11 +20,12 @@ import com.gmail.a93ak.andrei19.finance30.model.models.Purse;
 import org.achartengine.ChartFactory;
 import org.achartengine.GraphicalView;
 import org.achartengine.chart.PointStyle;
+import org.achartengine.model.TimeSeries;
 import org.achartengine.model.XYMultipleSeriesDataset;
-import org.achartengine.model.XYSeries;
 import org.achartengine.renderer.XYMultipleSeriesRenderer;
 import org.achartengine.renderer.XYSeriesRenderer;
 
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Random;
 
@@ -34,22 +38,37 @@ public class BalanceChartActivity extends AppCompatActivity implements OnTaskCom
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.balance_activity);
-//        spinner = (AppCompatSpinner) findViewById(R.id.pursesNamesBalanceChart);
-//        new PurseExecutor(this).execute(new RequestHolder<Purse>().getAllToList(RequestHolder.SELECTION_ALL));
-        createChart();
+        spinner = (AppCompatSpinner) findViewById(R.id.pursesNamesBalanceChart);
+        new PurseExecutor(this).execute(new RequestHolder<Purse>().getAllToList(RequestHolder.SELECTION_ALL));
+//        createChart();
     }
 
     private void createChart() {
-        final XYSeries series = new XYSeries("Balance");
+
+        final int days = 100;
         int sum = 500;
-        for (int day = 0; day < 500; day++) {
-            final Random r = new Random();
-            sum -= 2 * r.nextDouble();
-            series.add(day++, sum);
+        int max = 0;
+        int min = 500;
+
+        final TimeSeries series = new TimeSeries("Balance");
+        final Random r = new Random();
+        for (int i = 0; i < days; i++) {
+            final GregorianCalendar gc = new GregorianCalendar(2016, 10, i + 1);
+            sum -= 7 * r.nextDouble();
+            if (i % 30 == 0) {
+                sum += 250 * r.nextDouble();
+            }
+            if (sum > max) {
+                max = sum;
+            }
+            if (sum < min) {
+                min = sum;
+            }
+            series.add(gc.getTime(), sum);
         }
 
-        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
-        dataset.addSeries(series);
+        final XYMultipleSeriesDataset dataSet = new XYMultipleSeriesDataset();
+        dataSet.addSeries(series);
 
         final XYSeriesRenderer renderer = new XYSeriesRenderer();
         renderer.setLineWidth(1);
@@ -58,37 +77,32 @@ public class BalanceChartActivity extends AppCompatActivity implements OnTaskCom
         renderer.setPointStyle(PointStyle.POINT);
         renderer.setPointStrokeWidth(2);
 
-        XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
+        final XYMultipleSeriesRenderer mRenderer = new XYMultipleSeriesRenderer();
         mRenderer.setMarginsColor(Color.argb(0x00, 0xff, 0x00, 0x00));
         mRenderer.addSeriesRenderer(renderer);
         mRenderer.setPanEnabled(true, false);
-        final double[] panLimits = new double[4];
-        panLimits[0] = 0;
-        panLimits[1] = 500;
-        mRenderer.setPanLimits(panLimits);
         mRenderer.setZoomEnabled(true, false);
+        final double[] panLimits = new double[4];
+        panLimits[0] = new GregorianCalendar(2016, 10, 1).getTimeInMillis();
+        panLimits[1] = new GregorianCalendar(2016, 10, 1 + days).getTimeInMillis();
         final double[] zoomLimits = new double[4];
-        zoomLimits[0] = 0;
-        zoomLimits[1] = 500;
-        zoomLimits[2] = 0;
-        zoomLimits[3] = 0;
+        zoomLimits[0] = new GregorianCalendar(2016, 10, 1).getTimeInMillis();
+        zoomLimits[1] = new GregorianCalendar(2016, 10, 1 + days).getTimeInMillis();
+        mRenderer.setPanLimits(panLimits);
         mRenderer.setZoomLimits(zoomLimits);
 
-//        mRenderer.setPointSize(40);
-        mRenderer.setLabelsTextSize(40);
-        mRenderer.setLegendTextSize(40);
-        mRenderer.setAxisTitleTextSize(40);
-        mRenderer.setChartTitleTextSize(40);
-        mRenderer.setZoomInLimitX(30);
-        mRenderer.setYAxisMax(500);
-        mRenderer.setYAxisMin(0);
+        mRenderer.setZoomInLimitX(600000000);
+        mRenderer.setLabelsTextSize(35);
+        mRenderer.setAxisTitleTextSize(35);
+        mRenderer.setShowLegend(false);
         mRenderer.setShowGrid(true);
-        final GraphicalView chartView = ChartFactory.getLineChartView(this, dataset, mRenderer);
+        mRenderer.setYAxisMin(min);
+        mRenderer.setYAxisMax(max);
+        final GraphicalView chartView = ChartFactory.getTimeChartView(this, dataSet, mRenderer, "dd-MMM-yyyy");
         final LinearLayout layout = (LinearLayout) findViewById(R.id.chartBalance);
         layout.removeAllViews();
         layout.addView(chartView, 0);
     }
-
 
     @Override
     public void onTaskCompleted(final Result result) {
@@ -103,24 +117,17 @@ public class BalanceChartActivity extends AppCompatActivity implements OnTaskCom
             final ArrayAdapter<String> spinnerPursesAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, pursesNames);
             spinnerPursesAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spinner.setAdapter(spinnerPursesAdapter);
-//            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//                @Override
-//                public void onItemSelected(final AdapterView<?> parent, final View view, final int position, final long id) {
-//                    final long purseId = (pursesList.get(position).getId());
-////                    final Bundle args = new Bundle();
-////                    args.putLong(Income.PURSE_ID, purseId);
-////                    args.putBoolean(PieChartItem.TYPE, getIntent().getBooleanExtra(PieChartItem.TYPE, false));
-////                    args.putLong(Income.CATEGORY_ID, -1L);
-////                    getSupportLoaderManager().restartLoader(MAIN_LOADER, args, PieChartActivity.this);
-////                    final Loader<Object> loader = PieChartActivity.this.getSupportLoaderManager().getLoader(MAIN_LOADER);
-////                    loader.forceLoad();
-//                }
-//
-//                @Override
-//                public void onNothingSelected(final AdapterView<?> parent) {
-//
-//                }
-//            });
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(final AdapterView<?> parent, final View view, final int position, final long id) {
+                    createChart();
+                }
+
+                @Override
+                public void onNothingSelected(final AdapterView<?> parent) {
+
+                }
+            });
         }
     }
 }
